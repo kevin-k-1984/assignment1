8f6bc51c6e936f8129a0c959992a542bf331678c
Kevin <kkemp@Kemp-home-PC.attlocal.net>
2021-02-23 20:38:28 -0600

    Started building AccountHolder

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -2,4 +2,44 @@ package com.meritamerica.assignment1;
 
 public class AccountHolder {
 	
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	
+	public AccountHolder() {}
+	
+	public AccountHolder(String firstName, String middleName, String lastName, String ssn, 
+			double checkingAccountOpeningBalance, double savingsAccountOpeningBalance) {
+		
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 }
--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -2,4 +2,7 @@ package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
 	
+	public static void main(String[] args) {
+		
+	}
 }
