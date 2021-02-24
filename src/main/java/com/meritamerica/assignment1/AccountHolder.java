371f9e5cb9e878bb5d99fa912e336a17d6ebb28c
Kevin <kkemp@Kemp-home-PC.attlocal.net>
2021-02-23 22:50:53 -0600

    Adding AccountHolder getters and setters

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -5,15 +5,25 @@ public class AccountHolder {
 	private String firstName;
 	private String middleName;
 	private String lastName;
+	private String ssn;
+	private CheckingAccount checkingAccount;
+	private SavingsAccount savingsAccount;
 	
-	public AccountHolder() {}
+	public AccountHolder() {
+		firstName = "";
+		middleName = "";
+		lastName = "";
+		ssn = "";
+		checkingAccount = new CheckingAccount();
+		savingsAccount = new SavingsAccount();
+	}
 	
 	public AccountHolder(String firstName, String middleName, String lastName, String ssn, 
 			double checkingAccountOpeningBalance, double savingsAccountOpeningBalance) {
-		
 		this.firstName = firstName;
 		this.middleName = middleName;
 		this.lastName = lastName;
+		this.ssn = ssn;
 		
 	}
 
@@ -40,6 +50,21 @@ public class AccountHolder {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+//	public double getCheckingAccountBalance() {
+//		return checkingAccountBalance;
+//	}
+//
+//	public double getSavingsAccountBalance() {
+//		return savingsAccountBalance;
+//	}
 	
 }
