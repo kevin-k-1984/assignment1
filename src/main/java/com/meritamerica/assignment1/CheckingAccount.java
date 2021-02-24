7a6fdf2ac2db1c694b8ea9ece68791025792268b
Kevin <kkemp@Kemp-home-PC.attlocal.net>
2021-02-23 23:17:58 -0600

    Build out of basic methods

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -2,4 +2,34 @@ package com.meritamerica.assignment1;
 
 public class CheckingAccount {
 	
+	private double balance;
+	private double interestRate;
+	
+	public CheckingAccount() {
+		balance = 0.0;
+	}
+	
+	public CheckingAccount(double balance, double InterestRate) {
+		deposit(balance);
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(double amount) {
+		
+	}
+	
+	public void widthdraw(double amount) {
+		
+	}
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+	
+	public void setInterestRate(double rate) {
+		
+	}
 }
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -2,4 +2,36 @@ package com.meritamerica.assignment1;
 
 public class SavingsAccount {
 	
+	private double balance;
+	private double interestRate;
+	
+	public SavingsAccount() {
+		balance = 0.0;
+	}
+	
+	public SavingsAccount(double balance, double interestRate) {
+		deposit(balance);
+		
+		
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(double amount) {
+		
+	}
+	
+	public void widthdraw(double amount) {
+		
+	}
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+	
+	public void setInterestRate(double rate) {
+		
+	}
 }
