88b56cc25822bf89279c3c0bac66cc1f55d7157a
kevin_k <kkemp1984@gmail.com>
2021-02-25 15:50:08 -0600

    fixed the toString print issues

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -6,7 +6,7 @@ public class MeritAmericaBankApp {
 	public static void main(String[] args) {
 		
 		AccountHolder account1 = new AccountHolder("Michael","","","",100,1000);
-		account1.toString();
+		System.out.println(account1.toString());
 		account1.getCheckingAccount().deposit(500);
 		account1.getSavingsAccount().withdraw(800);
 		account1.getCheckingAccount().toString();
@@ -15,7 +15,7 @@ public class MeritAmericaBankApp {
 		AccountHolder account2 = new AccountHolder("Tom","","","",200,500);
 		account2.getCheckingAccount().deposit(-500);
 		account2.getSavingsAccount().withdraw(600);
-		account2.toString();
+		System.out.println(account2.toString());
 		
 	}
 }
