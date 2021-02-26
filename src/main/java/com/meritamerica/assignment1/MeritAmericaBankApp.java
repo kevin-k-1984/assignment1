18999c3beabf5a8d0262ace1819a22a70648609f
kevin_k <kkemp1984@gmail.com>
2021-02-26 16:05:27 -0600

    updated main method with printed toString calls

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -9,8 +9,8 @@ public class MeritAmericaBankApp {
 		System.out.println(account1.toString());
 		account1.getCheckingAccount().deposit(500);
 		account1.getSavingsAccount().withdraw(800);
-		account1.getCheckingAccount().toString();
-		account1.getSavingsAccount().toString();
+		System.out.println(account1.getCheckingAccount().toString());
+		System.out.println(account1.getSavingsAccount().toString());
 		
 		AccountHolder account2 = new AccountHolder("Tom","Dick","Herry","987-65-4321",200,500);
 		account2.getCheckingAccount().deposit(-500);
